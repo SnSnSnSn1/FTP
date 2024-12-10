be0e2c7422ecb7aaea0d4f0af06ed8c9f8ce2745
SnSnSnSn1 <3298564802@qq.com>
2024-12-10 14:59:12 +0800

    Delete src directory

--- src/include/http_server.h
+++
@@ -1,9 +0,0 @@
-#ifndef HTTP_SERVER_H
-#define HTTP_SERVER_H
-#include <stdint.h> 
-void http_server_init(uint16_t port);
-void http_server_run();
-void handle_request(int client_socket);
-char* get_content_type(char* filename);
-
-#endif
